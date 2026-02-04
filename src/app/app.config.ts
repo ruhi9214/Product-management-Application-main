2228611c1b51f0d9603bc2be8bf43dcbdf343a30
ruhi9214 <rohiniphadtare9202@gmail.com>
2026-02-04 11:42:25 +0530

    changes in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptor/auth.interceptor';
-=======
 
-import { routes } from './app.routes';
->>>>>>> da613fa8b1cd79a0137dd58c041f27da24aedfdb
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-<<<<<<< HEAD
     provideRouter(routes),
 
     // ✅ HttpClient + Interceptor
@@ -21,8 +17,4 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([authInterceptor])
     ),
   ],
-=======
-    provideRouter(routes)
-  ]
->>>>>>> da613fa8b1cd79a0137dd58c041f27da24aedfdb
 };
